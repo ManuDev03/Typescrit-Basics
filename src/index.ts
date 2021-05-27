20845f0e9cc518a0e544ff03c048b7bfd9577c61
ManuDev03 <manuprajan03@gmail.com>
2021-05-27 18:39:23 +0530

    Axios get url data

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
 import axios from 'axios';
 const url = 'https://jsonplaceholder.typicode.com/todos/1'
+interface Todo {
+    id:number;
+    title:string;
+    completed:true;
+}
 axios.get(url).then(response =>{
-    console.log(response.data)
+    const todo = response.data as Todo;
+    const id = todo.id;
+    const title =todo.title;
+    const finished = todo.completed;
+    console.log(`it has id ${id}, with a title ${title} and finished ${finished}`)
 })
 
+ 
